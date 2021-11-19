0bc806da475b5ed542a5b69eaaf29627bae160c4
Marco Obinu <marco.obinu@omegamadlab.com>
2021-11-19 21:41:41 +0100

    last update

--- demo.ps1
+++ demo.ps1
@@ -6,9 +6,7 @@ code .\01-BasicExamples\ultraSimpleSQL.png
 
 code .\01-BasicExamples\ultraSimpleSQL_source.json
 
-code .\01-BasicExamples\ultraSimpleSQL.bicep
-
-bicep build .\01-BasicExamples\ultraSimpleSQL.bicep
+bicep build .\01-BasicExamples\ultraSimpleSQL_final.bicep
 
 code -d .\01-BasicExamples\simpleSQL_source.json .\01-BasicExamples\simpleSQL_final.json
 
@@ -23,13 +21,14 @@ New-AzResourceGroupDeployment -ResourceGroupName SQLDEMO-RG -TemplateFile .\01-B
 # Dynamic approach
 code .\01-BasicExamples\simpleSQL.png
 code .\01-BasicExamples\simpleSQL_source.json
-code .\01-BasicExamples\simpleSQL.bicep
+code .\01-BasicExamples\simpleSQL_final.bicep
 
 code .\01-BasicExamples\webApp.png
 code .\01-BasicExamples\webApp.json
 code .\01-BasicExamples\webApp.bicep
 
 # Modules
+code .\01-BasicExamples\ultraSimpleSQL.png
 code .\03-Modules\sqlSrv-module.bicep
 code .\03-Modules\db-main.bicep
 
@@ -43,6 +42,9 @@ code .\03-Modules\sqlAndDb-condition.json
 code .\03-Modules\webApp_geo.png
 code .\03-Modules\webApp-module.bicep
 code .\03-Modules\webApp-main.bicep
+
+# Module registry
+code -d .\03-Modules\db-main-condition.bicep .\03-Modules\db-main-condition-reg.bicep
 
 
 
